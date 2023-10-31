f6602dbb39e6e68fee5955cdc346130eb3bcd0c0
Gill-DiogoOk <140555378+Gill-DiogoOk@users.noreply.github.com>
2023-10-31 14:27:34 -0400

    Add files via upload

---
+++ ex01.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Me livrando ai</title>
+</head>
+
+<body>
+    <h1>
+        <?php 
+        echo "Olá, Mundo! \u{1F30E}";
+        ?>
+    </h1>
+    
+    <p>Vamos tentar nos livrar da maldição do Maeda </p>
+
+    <?php 
+    echo "\u{1F30E} "
+    ?>
+
+</body>
+</html>
